ca4e721489495384d3da11378f8f197dcbf69a96
Matt Nedrich <mattnedrich@gmail.com>
2016-11-20 23:30:04 -0500

    initial version of median cut working

--- index.js
+++ index.js
@@ -44,36 +44,35 @@ function run(){
   var pixelData = canvas.getContext('2d').getImageData(0, 0, 1, 1).data;
   console.log("pixel data: " + pixelData);
 
-  var pixelsX = [];
-  var pixelsY = [];
-  var pixelsZ = [];
+  var pixels = [];
   for (var x=0; x < canvas.width; x++){
     for (var y=0; y < canvas.height; y++){
       var pixel = canvas.getContext('2d').getImageData(x, y, 1, 1).data;
-      pixelsX.push(pixel[0]);
-      pixelsY.push(pixel[1]);
-      pixelsZ.push(pixel[2]);
+      pixels.push({
+        red: pixel[0],
+        green: pixel[1],
+        blue: pixel[2]
+      });
     }
   }
-  plotWithPlotly(pixelsX, pixelsY, pixelsZ);
+  plotWithPlotly(pixels);
 }
 
-function plotWithPlotly(pixelsX, pixelsY, pixelsZ){
-  var colorData = _.map(pixelsX, function(x, index){
-    var xval = pixelsX[index];
-    var yval = pixelsY[index];
-    var zval = pixelsZ[index];
-    return "rgb(" + xval + "," + yval + "," + zval + ")";
+function plotWithPlotly(pixels){
+
+  console.log("will plot " + pixels.length + " pixels");
+  var colors = _.map(pixels, function(p, index){
+    return "rgb(" + p.red + "," + p.green + "," + p.blue + ")";
   });
 
   var data = {
-    x: pixelsX,
-    y: pixelsY,
-    z: pixelsZ,
+    x: _.map(pixels, function(p){ return p.red; }),
+    y: _.map(pixels, function(p){ return p.green; }),
+    z: _.map(pixels, function(p){ return p.blue; }),
     mode: 'markers',
     marker: {
       size: 5,
-      color: colorData,
+      color: colors,
       line: {
         color: 'rgb(100,100,100)',
         width: 1
@@ -86,7 +85,113 @@ function plotWithPlotly(pixelsX, pixelsY, pixelsZ){
     margin: { l:0, r:0, b: 0, t: 0 }
   };
 
-  Plotly.newPlot('plot', [data], layout);
+  // Plotly.newPlot('plot', [data], layout);
+
+  var groups = medianCut([pixels], 0, 10);
+
+  console.log("=======================")
+  _.each(groups, function(g){
+    console.log("Cluster: " + g.length + " points");
+  });
+  plotClusters(groups, "plot2");
 }
+
+function plotClusters(pointGroups, elementId){
+  var data = _.map(pointGroups, function(group){
+    return {
+      x: _.map(group, function(p){ return p.red; }),
+      y: _.map(group, function(p){ return p.green; }),
+      z: _.map(group, function(p){ return p.blue; }),
+      mode: 'markers',
+      marker: {
+        size: 2,
+        // line: {
+        //   color: 'rgb(100,100,100)',
+        //   width: 1
+        // }
+      },
+      type: 'scatter3d'
+    };
+  });
+
+  var layout = {
+    margin: { l:0, r:0, b: 0, t: 0 }
+  };
+
+  Plotly.newPlot(elementId, data, layout);
+}
+
+function medianCut(pointGroups, currentIteration, maxIterations){
+  console.log("running medianCut with " + pointGroups.length + " groups");
+  if (currentIteration > maxIterations) {
+    return pointGroups;
+  }
+
+  // determine which group has the most variation in some dimension
+  var groupToSplitInfo = determineGroupToSplit(pointGroups);
+  console.log("will split based on " + groupToSplitInfo.name);
+  splitGroups = splitGroup(groupToSplitInfo.group, groupToSplitInfo);
+
+  _.pullAt(pointGroups, groupToSplitInfo.groupIndex);
+  _.each(splitGroups, function(g) {
+    pointGroups.push(g);
+  });
+
+  return medianCut(pointGroups, currentIteration + 1, maxIterations);
+};
+
+function splitGroup(points, splitInfo) {
+  var sortedPoints = _.sortBy(points, function(p){
+    return p[splitInfo.name];
+  });
+  var medianIndex = parseInt(sortedPoints.length / 2);
+  var medianValue = sortedPoints[medianIndex][splitInfo.name];
+  var groupA = [];
+  var groupB = [];
+  _.each(points, function(p){
+    if (p[splitInfo.name] > medianValue){
+      groupA.push(p);
+    } else {
+      groupB.push(p);
+    }
+  });
+  return [groupA, groupB];
+};
+
+function determineGroupToSplit(groups) {
+  groupStats = _.map(groups, function(group, index){
+    var reds = _.map(group, function(point){ return point.red; });
+    var blues = _.map(group, function(point){ return point.blue; });
+    var greens = _.map(group, function(point){ return point.green; });
+
+    var stats = [];
+    stats.push({
+      groupIndex: index,
+      group: groups[index],
+      name: "red",
+      index: 0,
+      range: _.max(reds) - _.min(reds)
+    });
+    stats.push({
+      groupIndex: index,
+      group: groups[index],
+      name: "green",
+      index: 1,
+      range: _.max(greens) - _.min(greens)
+    });
+    stats.push({
+      groupIndex: index,
+      group: groups[index],
+      name: "blue",
+      index: 2,
+      range: _.max(blues) - _.min(blues)
+    });
+
+    return _.last(_.sortBy(stats, 'range'));
+  });
+
+  var groupToSplit = _.last(_.sortBy(groupStats, 'range'));
+  return groupToSplit;
+};
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
